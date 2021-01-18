501f5dfa463b89348585732638dc6df4ca4489c7
Jimmy Shah <jimmyshah83@gmail.com>
2021-01-18 12:28:49 -0500

    Added config server related changes

---
+++ applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/RegistrationServerConfig.java
@@ -0,0 +1,7 @@
+package io.pivotal.pal.tracker.timesheets;
+
+public interface RegistrationServerConfig {
+
+    String getEndpoint();
+    void setEndpoint(String endpoint);
+}
--- applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/TimesheetsApp.java
+++ applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/TimesheetsApp.java
@@ -3,6 +3,8 @@ package io.pivotal.pal.tracker.timesheets;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestOperations;
@@ -10,6 +12,7 @@ import org.springframework.web.client.RestOperations;
 import java.util.TimeZone;
 
 
+@EnableConfigurationProperties(RegistrationServerConfigProperties.class)
 @SpringBootApplication
 @ComponentScan({"io.pivotal.pal.tracker.timesheets", "io.pivotal.pal.tracker.restsupport"})
 public class TimesheetsApp {
@@ -22,8 +25,8 @@ public class TimesheetsApp {
     @Bean
     ProjectClient projectClient(
             RestOperations restOperations,
-            @Value("${registration.server.endpoint}") String registrationEndpoint
+            RegistrationServerConfigProperties endpointConfig
     ) {
-        return new ProjectClient(restOperations, registrationEndpoint);
+        return new ProjectClient(restOperations, endpointConfig);
     }
 }
